64ebfaf408a05d040b1118cbec4e62a4e4e294e0
Okan Hollander <okan.hollander@quality.nl>
2024-01-07 15:42:43 +0100

    reformat to render

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -42,10 +42,10 @@ def index(request):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
+        # response_data = render_to_string("challenges/challenge.html")
+        return render(request, "challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>This month does not exist.</h1>")
-    return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
