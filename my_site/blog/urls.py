ead86764b923c0a8438c74cc1783034d89740205
Okan Hollander <okan.hollander@quality.nl>
2024-01-09 09:00:41 +0100

    updated post-details page

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.posts, name="posts"),
-    path("posts/<slug:slug>", views.post_detail, name="post_detail"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail"),
 ]
