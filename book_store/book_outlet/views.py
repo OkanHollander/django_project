0f653a6c21d0213d278e7a8cbf2b3c19705973b6
Okan Hollander <okan.hollander@quality.nl>
2024-01-09 22:03:13 +0100

    updated slug in urls

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -8,12 +8,12 @@ def index(request):
     return render(request, 'book_outlet/index.html', {
         'books': books})
 
-def book_detail(request, book_id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404("Book not found")
-    book = get_object_or_404(Book, pk=book_id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_detail.html', {
         "title": book.title,
         "author": book.author,
