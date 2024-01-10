a8f4f5dfb21547118cc9948f819ae520e60ed970
Okan Hollander <okan.hollander@quality.nl>
2024-01-10 09:26:37 +0100

    updated with prepopulated fields for slugs

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Book
 
+# Register your classes here.
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
