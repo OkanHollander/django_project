2d1a4e29e8739a12d0ef425cce33c449609c1287
Okan Hollander <okan.hollander@quality.nl>
2024-01-10 09:56:34 +0100

    updated with author

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
 
 class Book(models.Model):
@@ -11,7 +14,7 @@ class Book(models.Model):
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="",blank=True, null=False, db_index=True)
 
