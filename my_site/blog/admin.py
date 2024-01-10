cc9c00f31d8217acfa3ad05ec333902967d67911
Okan Hollander <okan.hollander@quality.nl>
2024-01-10 20:51:53 +0100

    populated database

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email_address")
+    ordering = ["first_name", "last_name", "email_address"]
+    search_fields = ["first_name", "last_name", "email_address"]
+    list_filter = ["first_name", "last_name", "email_address"]
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "author", "slug")
+    ordering = ["title", "author", "slug"]
+    list_filter = ["title", "author", "slug"]
+
+class TagAdmin(admin.ModelAdmin):
+    ordering = ["caption"]
+    list_filter = ["caption"]
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.caption}"
+    
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+        ordering = ["first_name", "last_name", "email_address"]
+        unique_together = ("first_name", "last_name", "email_address")
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField(MinValueValidator(10))
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True ,related_name="posts")
+    slug = models.SlugField(unique=True, default="",blank=True, null=False, db_index=True)
+    image_name = models.CharField(max_length=100, null=True)
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ["title", "author", "slug"]
+        unique_together = ("title", "author", "slug")
+
+
