e67f9b896091ce9fd8dc7a3457ade8e8a785d070
Okan Hollander <okan.hollander@quality.nl>
2024-01-10 08:41:41 +0100

    Aggregation and ordering

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, get_object_or_404
+
 # from django.http import Http404
 from .models import Book
+from django.db.models import Avg
+
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
-    return render(request, 'book_outlet/index.html', {
-        'books': books})
+    books = Book.objects.all().order_by("title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
+    return render(
+        request,
+        "book_outlet/index.html",
+        {
+            "books": books,
+            "total_number_of_books": num_books,
+            "average_rating": avg_rating,
+        },
+    )
+
 
 def book_detail(request, slug):
     # try:
@@ -14,9 +28,13 @@ def book_detail(request, slug):
     # except:
     #     raise Http404("Book not found")
     book = get_object_or_404(Book, slug=slug)
-    return render(request, 'book_outlet/book_detail.html', {
-        "title": book.title,
-        "author": book.author,
-        "rating": book.rating,
-        "is_bestselling": book.is_bestselling,
-    })
+    return render(
+        request,
+        "book_outlet/book_detail.html",
+        {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "is_bestselling": book.is_bestselling,
+        },
+    )
