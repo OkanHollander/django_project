60d0ffa24d3936addbcdecffe928f926a13b2414
Okan Hollander <okan.hollander@quality.nl>
2024-01-11 20:15:49 +0100

    updated DetailView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.ReviewView.as_view(), name="review"),
     path("thank-you/", views.ThankYouView.as_view(), name="thank_you"),
     path("review-list/", views.ReviewListView.as_view(), name="review_list"),
-    path("single-review/<int:id>/", views.SingleReviewView.as_view(), name="single_review"),
+    path("single-review/<int:pk>/", views.SingleReviewView.as_view(), name="single_review"),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .forms import ReviewForm
 from .models import Review
 
@@ -41,18 +41,9 @@ class ReviewListView(ListView):
         data = base_query.filter(rating__gte=3)
         return data
 
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = self.kwargs.get("id")
-        selected_review = Review.objects.get(id=review_id)
-        context["review"] = selected_review
-        return context
-
-
-
+    model = Review
 
 
 # def reviewer(request):
