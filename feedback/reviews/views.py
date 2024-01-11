5008fcd8e3db6454999bd3fc6bffb72cc70338ad
Okan Hollander <okan.hollander@quality.nl>
2024-01-11 19:47:14 +0100

    added template view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic import TemplateView
 from .forms import ReviewForm
 from .models import Review
 
@@ -20,6 +21,15 @@ class ReviewView(View):
         return render(request, "reviews/review.html", {
         "form": form})
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Thank you for your review!"
+        return context
+    
+
 # def reviewer(request):
 #     if request.method == "POST":
 #         form = ReviewForm(request.POST)
@@ -35,5 +45,5 @@ class ReviewView(View):
 #     return render(request, "reviews/review.html", {
 #         "form": form})
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
