b337a140a9557a6f60cb2b8967e9e5296ace8ba3
Okan Hollander <okan.hollander@quality.nl>
2024-01-11 11:59:40 +0100

    added form controls

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=100)
+    user_name = forms.CharField(max_length=100,
+                                label="Your Name",
+                                error_messages={"required": "Please enter your name.",
+                                                "max_length": "Your name must be 100 characters or less."})
