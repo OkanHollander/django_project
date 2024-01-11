e9039ac425957303fffc70a5a795a9bd5078af0b
Okan Hollander <okan.hollander@quality.nl>
2024-01-11 12:50:59 +0100

    added database support

--- feedback/reviews/admin.py
+++ feedback/reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user_name','review_text', 'rating',)
+    list_filter = ('user_name','review_text', 'rating',)
+    search_fields = ('user_name',)
+
+admin.site.register(Review, ReviewAdmin)
--- feedback/reviews/models.py
+++ feedback/reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Review(models.Model):
+    user_name = models.CharField(max_length=100)
+    review_text = models.CharField(max_length=250)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.user_name
+    
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+        ordering = ["user_name"]
+
