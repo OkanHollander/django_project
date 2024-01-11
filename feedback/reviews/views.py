f31ccd1221ca0fcba698e92de23a3ab7075b8cb4
Okan Hollander <okan.hollander@quality.nl>
2024-01-11 11:06:14 +0100

    updated with verification

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,9 +5,11 @@ from django.http import HttpResponseRedirect
 def reviewer(request):
     if request.method == "POST":
         entered_username = request.POST.get("username")
+        if entered_username == "":
+            return render(request, "reviews/review.html", context={"error": True})
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", context={"error": False})
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
